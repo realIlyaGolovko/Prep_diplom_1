3e1b1d74d3b6e3090e50a614b0dc0530b2291432
realIlyaGolovko <real-golovko-ilya@yandex.ru>
2022-05-03 17:54:30 +0300

    add BurgerTest class

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.when;
 import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
@@ -54,7 +55,32 @@ public class BurgerTest implements SetUp {
     Assert.assertEquals("Incorrect price",1,burger.ingredients.get(1).price,0);
     }
     @Test
-    public void getPrice(){
-
+    public void getPriceReturnCorrectValue(){
+        float price = 10;
+        float expectedPrice = price * 2 + price;
+        when(bun.getPrice()).thenReturn(price);
+        when(ingredient.getPrice()).thenReturn(price);
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Assert.assertEquals("Incorrect price",expectedPrice,burger.getPrice(),0);
     }
+    @Test
+    public void getReceipt(){
+        String nameBun = "bulochka";
+        String nameIngredient = "ogurchik";
+        IngredientType ingredientType = FILLING;
+        String expectedReceipt = String.format("(==== %s ====)" + "\n" +
+                        "= %s %s =" + "\n" +
+                        "(==== %s ====)" + "\n" + "\n" +
+                        "Price: %f%n",
+                nameBun, ingredientType.toString().toLowerCase(), nameIngredient, nameBun, ingredient.getPrice());
+        burger.setBuns(bun);
+        when(bun.getName()).thenReturn(nameBun);
+        burger.addIngredient(ingredient);
+        when(ingredient.getName()).thenReturn(nameIngredient);
+        when(ingredient.getType()).thenReturn(ingredientType);
+        Assert.assertEquals(expectedReceipt, burger.getReceipt());
+    }
+
+
 }
